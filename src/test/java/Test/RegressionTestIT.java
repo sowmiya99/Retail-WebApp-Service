0d0b7468d41f51fa21229eddea3924ddaa168c67
sowmiya99 <sowmi.chriss@gmail.com>
2016-09-23 01:53:33 -0400

    Update RegressionTestIT.java

--- src/test/java/Test/RegressionTestIT.java
+++ src/test/java/Test/RegressionTestIT.java
@@ -14,9 +14,9 @@ public class RegressionTestIT {
   @Test
   public void searchTest() {
 	  
-	  System.setProperty("webdriver.chrome.driver", "/usr/bin/firefox");
+	  System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
 	  WebDriver	driver = new ChromeDriver();
-	  driver.get("http://localhost:8080/");
+	  driver.get("http://54.226.235.226:8085/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
 	  
 	  loginPage loginpage = new loginPage(driver);
