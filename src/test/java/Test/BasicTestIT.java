8b7f08f13e21cd163b18d66d89fabcd11ea2752c
sowmiya99 <sowmi.chriss@gmail.com>
2016-09-23 14:23:00 +0530

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicTestIT {
   @Test
   public void f() {
-	  System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+	  System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
 	  WebDriver	driver = new ChromeDriver();
 	  driver.get("http://54.226.235.226:8085/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
