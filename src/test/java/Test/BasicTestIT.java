7be6e2ab7a1414643d016ee7b312080bad0e9106
sowmiya99 <sowmi.chriss@gmail.com>
2016-09-23 16:23:49 +0530

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 //import org.openqa.selenium.*;
 //import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.*; 
 
 public class BasicTestIT {
