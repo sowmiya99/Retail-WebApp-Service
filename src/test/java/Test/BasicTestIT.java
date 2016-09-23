9960c48a44876ce776ae829d34fc386d3bd190ee
sowmiya99 <sowmi.chriss@gmail.com>
2016-09-23 16:13:46 +0530

    Update BasicTestIT.java

--- src/test/java/Test/BasicTestIT.java
+++ src/test/java/Test/BasicTestIT.java
@@ -4,13 +4,15 @@ import junit.framework.Assert;
 
 import org.testng.annotations.Test;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.*;
 
 public class BasicTestIT {
   @Test
   public void f() {
-	  System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
-	  WebDriver	driver = new ChromeDriver();
+	 // System.setProperty("webdriver.chrome.driver", "/bin/chromedriver");
+	 // WebDriver	driver = new ChromeDriver();
+	 WebDriver	driver = new FirefoxDriver();
 	  driver.get("http://retailapp.cogpcfdevops.com/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
 	  driver.close();
